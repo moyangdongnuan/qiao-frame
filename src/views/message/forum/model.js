a1ef0f345ca997dc58ab28ffc28e40b98793a3c0
moyangdongnuan <987090365@qq.com>
2018-06-03 15:47:03 +0800

    完成 forum模块中留言分类的下拉列表

--- src/views/message/forum/model.js
+++ src/views/message/forum/model.js
@@ -2,7 +2,8 @@ const FormModel = {
   id: null,
   idcard: null, // 发帖人别名
   title: null, // 发帖标题
-  menuId: null, // 发帖内容
+  content: null, // 发帖内容
+  menuName: null, // 留言分类（菜单名称）
   category: null, // 审核标识
   categorytype: null // 类型标识
 }
